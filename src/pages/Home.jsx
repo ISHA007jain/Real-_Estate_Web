0fd99901c9f6ee3c14f9a195d9f9d22f272db07c
ankit-4623 <ankitnayek007@gmail.com>
2025-03-22 22:05:38 +0530

    modify Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import About from "./About";
 import { Aim } from "./Aim";
-import Lease_prop from "./Lease_prop";
+import Frontpage from "./Frontpage";
+import {Lease_prop} from "./Lease_prop";
 import Sale_prop from "./Sale_prop"
 
 export const Home = () => {
   return (
-    <>
+    <div className="w-full overflow-hidden">
+    <Frontpage/>
     <About />
     <Sale_prop />
     <Lease_prop />
     <Aim/>
-    
-    
-    </>
+    </div>
 
   );
 };
